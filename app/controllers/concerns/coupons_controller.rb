d9b70fbcdcbad022bb45f83ace20f24f81dc9c64
Daniel Suarez <suarez.daniel101@gmail.com>
2022-08-30 21:50:28 -0400

    Done.

--- app/controllers/concerns/coupons_controller.rb
+++ app/controllers/concerns/coupons_controller.rb
@@ -1,8 +1,19 @@
 class CouponsController < ApplicationController
   
-  def create
-    Coupon.create(coupon_code: params[:coupon][:coupon_code], store: params[:coupon][:store])
+  def index
+    @coupons = Coupon.all
+  end
 
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+  
+  def new
+  end
+  
+  def create
+   @coupon = Coupon.create(coupon_code: params[:coupon_code], store: params[:store])
+    redirect_to coupon_path(@coupon)
   end
 
 end
